71781e7887c89e131fa1b9e03e0b695505e94d18
AS608741 <AS608741@MSATBSD-LP01027.gegdc.ad>
2020-07-08 15:13:31 +0530

    Initial commite

---
+++ FSD/src/main/java/com/group2/FSD/FsdBankApplication.java
@@ -0,0 +1,13 @@
+package com.group2.FSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdBankApplication.class, args);
+	}
+
+}
