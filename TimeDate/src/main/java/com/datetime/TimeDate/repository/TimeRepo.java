d8d030f3ca30b8c72926745a88632bfc78900a22
AS608741 <AS608741@MSATBSD-LP01027.gegdc.ad>
2020-08-12 10:53:18 +0530

    fdgdf

--- TimeDate/src/main/java/com/datetime/TimeDate/repository/TimeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.datetime.TimeDate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.datetime.TimeDate.domain.TimeDate;
-
-public interface TimeRepo extends JpaRepository<TimeDate, Integer> {
-
-}
